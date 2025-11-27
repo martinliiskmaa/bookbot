281944cdd7308b34409578f21eeeb18c9f4d0601
martinliiskmaa <martinlametlik@gmail.com>
2025-11-27 19:44:09 +0200

    finalized commit. Added 2 books

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
+import sys
+from stats import get_book_text
 from stats import number_of_words
 from stats import letter_times
+from stats import chars_dict_to_sorted_list
+
 
 
 def main():
-    FILEPATH = "books/frankenstein.txt"
-    data = get_book_text(FILEPATH)
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    filepath = sys.argv[1]
+
+    data = get_book_text(filepath)
     word_count = number_of_words(data)
     print(f"Found {word_count} total words")
     char_dict = letter_times(data)
+    sorted_chars = chars_dict_to_sorted_list(char_dict)
+    for item in sorted_chars:
+        ch = item["char"]
+        if ch.isalpha():
+            print(f"{ch}: {item['num']}")
     return data
-
-
-def get_book_text(filepath):
-    with open(filepath, "r") as file:
-        data = file.read()
-    return data
-
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,9 @@
+def get_book_text(filepath):
+    with open(filepath, "r") as file:
+        data = file.read()
+    return data
+
+
 def number_of_words(data):
     num = len(data.split())
     return num
@@ -5,20 +11,29 @@ def number_of_words(data):
 
 def letter_times(data):
     data = data.lower()
-    char_dict = {}
+    char_dict = {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0,
+                 'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0,
+                 'k': 0, 'l': 0, 'm': 0, 'n': 0, 'o': 0,
+                 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0,
+                 'u': 0, 'v': 0,}
     for char in data:
         if char in char_dict:
             char_dict[char] += 1
         else:
             char_dict[char] = 1
+
     return char_dict
 
 
-def sorting(char_dict):
-    new_list = []
-    for char, count in char_dict.items():
-        if char in char_dict:
-            new_list.append(char)
+def sort_on(d):
+    return d["num"]
+
+def chars_dict_to_sorted_list(char_dict):
+    sorted_list = []
+    for ch in char_dict:
+        sorted_list.append({"char": ch, "num": char_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
 
 
 
